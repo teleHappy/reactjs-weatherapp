e8a6ea7bf341ed4aa8414f0013590830f43de4d6
Rick Dillon <Rick_Dillon@bose.com>
2016-12-30 11:26:10 -0500

    removed un-needed conditionals in render

--- src/app/components/Current.js
+++ src/app/components/Current.js
@@ -13,15 +13,14 @@ class Current extends React.Component{
 		<p className="date">{this.props.data.obDateTime} </p>
 		
 		<div className="highlight">
-		    <div className="temp">{(this.props.data.ob) ? this.props.data.ob.tempF: '--'}<sup>&deg;F</sup></div>
+		    <div className="temp">{ this.props.data.ob.tempF }<sup>&deg;F</sup></div>
 		    <div className="icon">
 		    <img 
-		    	src={(this.props.data.ob && this.props.data.ob.icon) ? "../images/AerisIcons/Aeris_WxIcons_55x55/" + this.props.data.ob.icon  : ''}
-		    	style={{display: (this.props.data.ob && this.props.data.ob.icon) ? "block" : "none"  }}
+		    	src={ "../images/AerisIcons/Aeris_WxIcons_55x55/" + this.props.data.ob.icon }
 		    	width="55" 
 		    	height="55" 
-		    	alt={(this.props.data.ob && this.props.data.ob.weatherShort) ? this.props.data.ob.weatherShort: '--'} 
-		    	title={(this.props.data.ob && this.props.data.ob.weatherShort) ? this.props.data.ob.weatherShort: '--'}
+		    	alt={ this.props.data.ob.weatherShort } 
+		    	title={ this.props.data.ob.weatherShort }
 		    />
 
 		    </div>
